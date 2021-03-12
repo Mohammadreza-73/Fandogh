acfa77b9f66e8466eaaef3be0e8578366d7cb27a
Mohammadreza-73 <mohammadreza.rahimi93@yahoo.com>
2021-03-12 13:31:23 +0330

    modify-current-method-and-add-segments-&-queryParameters-methods

--- App/Utilities/Url.php
+++ App/Utilities/Url.php
@@ -4,23 +4,42 @@ namespace App\Utilities;
 
 class Url
 {
-    public static function current()
+    public static function current(): string
     {
-        return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     }
 
-    public static function currentRoute()
+    public static function currentRoute(): string
     {
         return strtok($_SERVER['REQUEST_URI'], '?');
     }
 
-    public static function queryParameters(string $url)
+    /**
+     * Parse current URL and return its components
+     * 
+     * component: retrieve just a specific URL component as a string
+     * 
+     * like: PHP_URL_SCHEME, PHP_URL_HOST, PHP_URL_PORT, PHP_URL_USER,
+     *       PHP_URL_PASS, PHP_URL_PATH, PHP_URL_QUERY or PHP_URL_FRAGMENT
+     * 
+     * @param string $components
+     * @return void
+     */
+    public static function segments(string $component = null)
     {
-        // TODO: Impelement
+        if ( !isset($component) )
+            return parse_url(self::current());
+
+        return parse_url(self::current(), $component);
     }
 
-    public static function segments(string $url)
+    /**
+     * Retrieve current URL Query Parameters
+     *
+     * @return void
+     */
+    public static function queryParameters()
     {
-        // TODO: Impelement
+        return self::segments(PHP_URL_QUERY);
     }
 }
