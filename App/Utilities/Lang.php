719815dbc089f7b4745edb3192ecf7f4ec6b8391
Mohammadreza-73 <mohammadreza.rahimi93@yahoo.com>
2021-03-12 13:28:23 +0330

    add-language-utility-for-convert-numbers

--- App/Utilities/Lang.php
+++ App/Utilities/Lang.php
@@ -6,7 +6,7 @@ class Lang
 {
     public static function persianNumbers($input)
     {
-        $fa_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        $fa_num = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
         $en_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
         return str_replace($en_num, $fa_num, $input);
@@ -14,7 +14,7 @@ class Lang
 
     public static function latinNumbers($input)
     {
-        $fa_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        $fa_num = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
         $en_num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
         return str_replace($fa_num, $en_num, $input);
