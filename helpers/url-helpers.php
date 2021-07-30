93632e87e928b570b0d8b785f11139926be824f9
Mohammadreza-73 <mohammadreza.rahimi93@yahoo.com>
2021-07-30 15:51:57 +0430

    Add function for view

--- helpers/url-helpers.php
+++ helpers/url-helpers.php
@@ -15,3 +15,11 @@ function randomElement(array $array)
     shuffle($array);
     return array_pop($array);
 }
+
+function view(string $path, array $data = []): void
+{
+    extract($data);
+    $path = str_replace('.', '/', $path);
+    include BASE_PATH . "/views/{$path}.php";
+    die();
+}
