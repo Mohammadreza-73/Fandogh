9b6fac09c827cce4358bb05657d09872433e3ae0
Mohammadreza-73 <mohammadreza.rahimi93@yahoo.com>
2021-08-10 10:09:09 +0430

    Add Controller routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,32 @@
 
 use App\Core\Routing\Route;
 
-Route::add(['get', 'post'] ,'/auth', function() {
+Route::add(['get', 'post'], '/auth', function () {
     echo 'Hello';
 });
 
-Route::add(['get'] ,'/null');
+Route::add(['get'], '/null');
 
-Route::add(['post'] ,'/enroll', function() {
+Route::add(['post'], '/enroll', function () {
     echo 'Hello';
 });
 
-Route::get('/greeting', function() {
-    echo 'Hello';
+Route::get('/greeting', function () {
+    echo 'Hello from greeting';
 });
 
-Route::post('/saveForm', function() {
+Route::post('/saveForm', function () {
     echo 'Form submit';
 });
 
-// Route::put('/put-uri', ['UrlController', 'index']);
-// Route::patch('/patch-uri', ['UrlController@index']);
+Route::get('/', ['HomeController', 'index']);
+
+Route::get('/home', 'HomeController@order');
+
+Route::get('/archive', 'ArchiveController@index');
+
+Route::get('/archive/products', 'ArchiveController@products');
+
+Route::get('/archive/articles', 'ArchiveController@articles');
+
+Route::get('/todo/list', 'TodoController@list');
