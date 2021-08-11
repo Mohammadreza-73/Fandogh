fdd78bf0733b7edba43901016a2591e69403dcc6
Mohammadreza-73 <mohammadreza.rahimi93@yahoo.com>
2021-08-11 16:20:56 +0430

    Add background color to 404 page

--- views/errors/404.php
+++ views/errors/404.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 Not Found</title>
 </head>
-<body>
-    <h1 style="color: red">Oops. page not found!</h1>
+<body style="background:#f1f1f1">
+    <h1 style="text-align: center">404: Page Not Found!</h1>
 </body>
 </html>
