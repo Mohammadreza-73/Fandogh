a35c8a8ebde66b96996e8b4ed36d3609a275a0c3
Mohammadreza-73 <mohammadreza.rahimi93@yahoo.com>
2021-08-16 15:17:21 +0430

    Modify 404 page style

--- views/errors/404.php
+++ views/errors/404.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 Not Found</title>
+    <title>404 | Not Found</title>
+    <style>
+        body {
+            font-family: 'Open Sans', sans-serif;
+            padding: 0;
+            margin: 0;
+            background: #263238;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        h2 {
+            color: #BDBDBD;
+        }
+    </style>
 </head>
-<body style="background:#f1f1f1">
-    <h1 style="text-align: center">404: Page Not Found!</h1>
+<body>
+    <h2>404 | PAGE NOT FOUND</h2>
 </body>
 </html>
