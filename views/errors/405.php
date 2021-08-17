a4de8f3ea271619f838e2ed8301781d029397953
Mohammadreza-73 <mohammadreza.rahimi93@yahoo.com>
2021-08-17 10:25:33 +0430

    Modify 405 page style

---
+++ views/errors/405.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>405 | Method Not Allowed</title>
+    <style>
+        body {
+            font-family: 'Open Sans', sans-serif;
+            padding: 0;
+            margin: 0;
+            background: #263238;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        h2 {
+            color: #BDBDBD;
+        }
+    </style>
+</head>
+<body>
+    <h2>405 | INVALID METHOD</h2>
+</body>
+</html>
