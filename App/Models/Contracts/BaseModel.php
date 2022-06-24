f4ae34c4a244938da404bca2f78f09ec4f183369
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2022-06-24 19:52:16 +0430

    Add BaseModel in contracts.

---
+++ App/Models/Contracts/BaseModel.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models\Contracts;
+
+abstract class BaseModel implements CrudInterface
+{
+    /**
+     * Database connection.
+     *
+     * @var string
+     */
+    protected $connection;
+
+    /**
+     * Table name.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * Table primary key.
+     *
+     * @var string
+     */
+    protected $primary_key = 'id';
+
+    /**
+     * Page size of result.
+     *
+     * @var integer
+     */
+    protected $per_page = 15;
+
+    /**
+     * Table column name.
+     *
+     * @var array
+     */
+    protected $attributes = [];
+
+    protected function __construct()
+    {
+
+    }
+
+    /**
+     * Get a value of a column from table.
+     *
+     * @param string $key
+     * @return string|null
+     */
+    protected function getAttributes(string $key): ?string
+    {
+        if (! isset($key) || ! array_key_exists($key, $this->attributes)) {
+            return null;
+        }
+
+        return $this->attributes[$key];
+    }
+}
