9bd9e2601f06efefbc0e38380a571c767601730b
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2022-07-01 23:34:09 +0430

    Modify Medoo database connection.

--- App/Models/Contracts/MysqlBaseModel.php
+++ App/Models/Contracts/MysqlBaseModel.php
@@ -9,41 +9,35 @@ class MysqlBaseModel extends BaseModel
 {
     public function __construct()
     {
-        try {
-            $this->connection = new Medoo([
-                'type' => $_ENV['DB_CONNECTION'],
-                'host' => $_ENV['DB_HOST'],
-                'database' => $_ENV['DB_DATABASE'],
-                'username' => $_ENV['DB_USERNAME'],
-                'password' => $_ENV['DB_PASSWORD'],
-             
-                'charset' => $_ENV['DB_CHARSET'],
-                'collation' => 'utf8mb4_general_ci',
-                'port' => $_ENV['DB_PORT'],
-             
-                'prefix' => $_ENV['DB_PREFIX'],
-                'logging' => $_ENV['DB_LOGGING'],
-             
-                /**
-                 * @example PDO::ERRMODE_SILENT (default) | PDO::ERRMODE_WARNING | PDO::ERRMODE_EXCEPTION
-                 * @see Read more from https://www.php.net/manual/en/pdo.error-handling.php.
-                 */
-                'error' => \PDO::ERRMODE_EXCEPTION,
-             
-                /**
-                 * The driver_option for connection.
-                 * 
-                 * @see Read more from http://www.php.net/manual/en/pdo.setattribute.php.
-                 */
-                'option' => [
-                    // PDO::ATTR_CASE => PDO::CASE_NATURAL
-                ],
-            ]);
-
-        } catch (\PDOException $e) {
-            die('Database Connection Failed: ' . $e->getMessage());
-        }
-        
+        $this->connection = new Medoo([
+            'type' => $_ENV['DB_CONNECTION'],
+            'host' => $_ENV['DB_HOST'],
+            'database' => $_ENV['DB_DATABASE'],
+            'username' => $_ENV['DB_USERNAME'],
+            'password' => $_ENV['DB_PASSWORD'],
+            
+            'charset' => $_ENV['DB_CHARSET'],
+            'collation' => 'utf8mb4_general_ci',
+            'port' => $_ENV['DB_PORT'],
+            
+            'prefix' => $_ENV['DB_PREFIX'],
+            'logging' => $_ENV['DB_LOGGING'],
+            
+            /**
+             * @example PDO::ERRMODE_SILENT (default) | PDO::ERRMODE_WARNING | PDO::ERRMODE_EXCEPTION
+             * @see Read more from https://www.php.net/manual/en/pdo.error-handling.php.
+             */
+            'error' => \PDO::ERRMODE_EXCEPTION,
+            
+            /**
+             * The driver_option for connection.
+             * 
+             * @see Read more from http://www.php.net/manual/en/pdo.setattribute.php.
+             */
+            'option' => [
+                // PDO::ATTR_CASE => PDO::CASE_NATURAL
+            ],
+        ]);
     }
 
     public function create(array $data): int
