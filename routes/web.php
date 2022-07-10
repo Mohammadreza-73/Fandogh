a2b4260470e83e74a98147372e389e6c9bc85b3f
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2022-07-10 19:30:35 +0430

    Remove useless files:
    Remove useless controllers,
    Remove useless models,
    Remvoe useless views,
    Remove useless routes.

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,9 @@ use App\Core\Routing\Route;
 use App\Middleware\BlockFirefox;
 
 Route::get('/', function () {
-    echo 'Welcome Page';
+    return view('welcome');
 });
-
-Route::add(['get'], '/null');
-
-Route::get('/home', ['HomeController', 'index']);
 
 Route::get('/post/{slug}/comment/{cid}', 'PostController@single');
 
-Route::get('/order', 'HomeController@order');
-
-Route::get('/archive', 'ArchiveController@index');
-
-Route::get('/archive/products', 'ArchiveController@products');
-
-Route::get('/archive/articles', 'ArchiveController@articles');
-
 Route::get('/todo/list', 'TodoController@list', [BlockFirefox::class]);
-
-Route::any('/test', function () {
-    echo 'test route';
-});
