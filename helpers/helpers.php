d8605bd6299af01a5abb92e508d08b92a2751c50
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2022-07-10 20:54:05 +0430

    Add app config file:
    Add new configs to .env,
    Add env helper function,
    Add some config into app config.

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -105,3 +105,17 @@ if (! function_exists('strContains'))
         return ($pos !== false) ? true : false;
     }
 }
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        return $_ENV[$key] ?? $default;
+    }
+}
