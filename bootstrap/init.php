433b98e21978648d6deef8414abf3e612bd8b5e5
Mohammadreza_73 <mohammadreza.rahimi93@yahoo.com>
2023-01-20 21:15:18 +0330

    Add flip/whoops package:
    Modify error page with this package.

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -6,6 +6,10 @@ include BASE_PATH . "/vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 $request = new \App\Core\Request();
 
 include BASE_PATH . "/routes/web.php";
